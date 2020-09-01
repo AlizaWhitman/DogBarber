badaed3570961b8543b418b916a57af2e3ce2b2c
Aliza Whitman <alizawhitman17@gmail.com>
2020-09-01 13:12:40 +0300

    Add files via upload

---
+++ Server Side/DogBarber/Startup.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BL;
+using DL;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace DogBarber
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddDbContext<dogBarberShopDBContext>(options => options.UseSqlServer("Server = DESKTOP-2GVI95M; Database = dogBarberShopDB; Trusted_Connection = True;"));
+            services.AddScoped(typeof(IClientsBL), typeof(ClientsBL));
+            services.AddScoped(typeof(IQueueBL), typeof(QueueBL));
+            services.AddScoped(typeof(IClientsDL), typeof(ClientsDL));
+            services.AddScoped(typeof(IQueueDL), typeof(QueueDL));
+            services.AddCors();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseCors(options =>
+            options.WithOrigins("http://localhost:3000")
+            .AllowAnyHeader()
+            .AllowAnyMethod());
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
