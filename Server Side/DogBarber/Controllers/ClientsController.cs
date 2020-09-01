e6d0811522c14025f528b3d46235bb03291a1a81
Aliza Whitman <alizawhitman17@gmail.com>
2020-09-01 12:30:54 +0300

    Add files via upload

---
+++ Server Side/DogBarber/Controllers/ClientsController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BL;
+using Entities;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DogBarber.Controllers
+{
+    [Route("api/[controller]")]
+    public class ClientsController : Controller
+    {
+        private readonly IClientsBL _IClientsBL;
+        public ClientsController(IClientsBL IClientsBL)
+        {
+            _IClientsBL = IClientsBL;
+        }
+
+        // GET api/<controller>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        [HttpPost]
+        public async Task<ActionResult<Clients>> Post([FromBody]Clients newClient )
+        {
+            return await _IClientsBL.PostClient(newClient);
+
+        }
+
+        //[HttpGet("[action]{Clients}")]
+        [HttpPost ("LogIn")]
+        public async Task<ActionResult<Clients>> LogIn([FromBody]Clients client)
+        {
+            Console.WriteLine( "d");
+            return await _IClientsBL.GetClient(client);
+        }
+
+        // PUT api/<controller>/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, Clients UpdatedClient)
+        {
+            return await _IClientsBL.PutClient(id, UpdatedClient);
+        }
+
+        // DELETE api/<controller>/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Clients>> Delete(int id)
+        {
+            return await _IClientsBL.DeleteClient(id);
+        }
+    }
+}
---
+++ Server Side/DogBarber/Controllers/QueueController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BL;
+using Entities;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DogBarber.Controllers
+{
+    [Route("api/[controller]")]
+    public class QueueController : Controller
+    {
+        private readonly IQueueBL _IQueueBL;
+        public QueueController(IQueueBL IQueueBL)
+        {
+            _IQueueBL = IQueueBL;
+        }
+
+        // GET: api/<controller>
+        [HttpGet]
+        public Task<ActionResult<IEnumerable<AppointmentDetails>>> Get()
+        {
+            return _IQueueBL.GetQueue();
+        }
+
+        // GET: api/<controller>/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Queue>> GetAppointment(int id)
+        {
+            return await _IQueueBL.GetAppointment(id);
+
+        }
+        // POST api/<controller>
+        [HttpPost]
+        public async Task<ActionResult<AppointmentDetails>> Post([FromBody]AppointmentDetails newQueue)
+        {
+            Queue queue = new Queue();
+            queue.id = newQueue.id;
+            queue.clientId = newQueue.clientId;
+            queue.bookingHour = newQueue.bookingHour;
+            queue.appointmentHour = newQueue.appointmentHour;
+            return await _IQueueBL.PostQueue(queue);
+
+        }
+
+        // PUT api/<controller>/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, [FromBody]AppointmentDetails UpdatedQueue)
+        {
+            Queue queue = new Queue();
+            queue.id = UpdatedQueue.id;
+            queue.clientId = UpdatedQueue.clientId;
+            queue.bookingHour = UpdatedQueue.bookingHour;
+            queue.appointmentHour = UpdatedQueue.appointmentHour;
+            return await _IQueueBL.PutQueue(id, queue);
+        }
+
+        // DELETE api/<controller>/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Queue>> Delete(int id)
+        {
+            return await _IQueueBL.DeleteQueue(id);
+        }
+    }
+}
