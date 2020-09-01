73cdd47beb31cceae4774be65f4d92cf0f674de3
Aliza Whitman <alizawhitman17@gmail.com>
2020-09-01 13:15:44 +0300

    Add files via upload

---
+++ Server Side/BL/QueueBL.cs
@@ -0,0 +1,44 @@
+﻿using DL;
+using Entities;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public class QueueBL : IQueueBL
+    {
+        IQueueDL _IQueueDL;
+        public QueueBL(IQueueDL IQueueDL)
+        {
+            _IQueueDL = IQueueDL;
+        }
+        public async Task<ActionResult<Queue>> DeleteQueue(int id)
+        {
+            return await _IQueueDL.DeleteQueue(id);
+        }
+
+        public async Task<ActionResult<Queue>> GetAppointment(int id)
+        {
+            return await _IQueueDL.GetAppointment(id);
+        }
+
+        public Task<ActionResult<IEnumerable<AppointmentDetails>>> GetQueue()
+        {
+            return _IQueueDL.GetQueue();
+        }
+
+        public async Task<ActionResult<AppointmentDetails>> PostQueue(Queue newQueue)
+        {
+            return await _IQueueDL.PostQueue(newQueue);
+        }
+        public async Task<IActionResult> PutQueue(int id, Queue updatedQueue)
+        {
+            return await _IQueueDL.PutQueue(id, updatedQueue);
+        }
+
+
+    }
+}
