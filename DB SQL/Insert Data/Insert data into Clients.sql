04ad25c2b308b1dcc73418009e1996343ab11e1d
Aliza Whitman <alizawhitman17@gmail.com>
2020-09-03 12:09:51 +0300

    Add files via upload

---
+++ DB SQL/Insert Data/Insert data into Clients.sql
@@ -0,0 +1,34 @@
+USE [dogBarberShopDB]
+GO
+
+INSERT INTO [dbo].[Clients]
+           ([FirstName]
+           ,[UserName]
+           ,[Password])
+     VALUES						
+	 ('Mary','Mary','1'),
+('Patricia ', 	'Patricia',  	'2   '  ),
+('Jennifer',  	'Jennifer',  	'3     '),   
+('Linda',  	'Linda   ',  	'4     ')    ,
+('Elizabeth',	'Elizabeth', 	'5     '),   
+('Barbara',  	'Barbara ',  	'6     '),   
+('Susan',  	'Susan   ',  	'7     ')    ,
+('Aliza',  	'Aliza   ',  	'8     ')    ,
+('Jessica',  	'Jessica ',  	'9     '),   
+('Sarah',  	'Sarah   ',  	'10    ')    ,
+('Karen',  	'Karen   ',  	'11    ')    ,
+('Nancy',  	'Nancy   ',  	'12    ')    ,
+('Margaret',  	'Margaret',  	'13    '),   
+('Lisa',  	'Lisa    ',  	'14    ')    ,
+('Betty',  	'Betty   ',  	'15    ')    ,
+('Mariam',  	'Mariam  ',  	'16    '),   
+('Yana',  	'Yana    ',  	'17    ')    ,
+('Fredy',  	'Fredy   ',  	'18    ')    ,
+('Warry',  	'Warry   ',  	'19    ')    ,
+('Prezy',  	'Prezy   ',  	'20    ')    ,
+('Grate',  	'Grate   ',  	'21    ')    ,
+('Jennifer',  	'Jennifer',  	'22    ') ,  
+	 
+GO
+
+
